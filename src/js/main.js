07441977a2fce342cda5e3bb886c273c12e953de
batyril <237x237@gmail.ru>
2022-08-12 15:13:45 +0300

    add eslint

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,17 @@
-const  numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
-const  lastWatchFilm_1 = prompt('Один из последних просмотренных фильмов?');
-const  ratingFilm_1 = +prompt('На сколько оцените его?');
-
-const  lastWatchFilm_2 = prompt('Один из последних просмотренных фильмов?')
-const  ratingFilm_2 = +prompt('На сколько оцените его?')
-
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
+const lastWatchFilm1 = prompt('Один из последних просмотренных фильмов?');
+const ratingFilm1 = +prompt('На сколько оцените его?');
+const lastWatchFilm2 = prompt('Один из последних просмотренных фильмов?');
+const ratingFilm2 = +prompt('На сколько оцените его?');
 
 const personalMovieDB = {
   count: numberOfFilms,
   movies: {},
   actors: {},
   genres: {},
-  private: false
+  private: false,
 };
 
-personalMovieDB.movies[lastWatchFilm_1] = ratingFilm_1;
+personalMovieDB.movies[lastWatchFilm1] = ratingFilm1;
 
-personalMovieDB.movies[lastWatchFilm_2] = ratingFilm_2
-
+personalMovieDB.movies[lastWatchFilm2] = ratingFilm2;
